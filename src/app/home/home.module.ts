7fcfd30694780851179b17169bd453c8a725973c
danielsilvalima1996 <dsl15021996@gmail.com>
2020-10-31 00:54:39 -0300

    adicionado codigo home

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -1,17 +1,15 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
+import { NgModule } from '@angular/core';
 import { HomeRoutingModule } from './home-routing.module';
 import { HomeComponent } from './home/home.component';
-import { PoPageModule } from '@po-ui/ng-components';
+
 
 
 @NgModule({
   declarations: [HomeComponent],
   imports: [
     CommonModule,
-    HomeRoutingModule,
-    PoPageModule
+    HomeRoutingModule
   ]
 })
 export class HomeModule { }
