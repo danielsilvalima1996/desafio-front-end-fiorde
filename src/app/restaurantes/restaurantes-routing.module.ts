755dd68450dd90f74e6ef6cc74ec495b6eec72db
danielsilvalima1996 <dsl15021996@gmail.com>
2020-11-01 18:09:05 -0300

    adicionado rotas e imports module

--- src/app/restaurantes/restaurantes-routing.module.ts
+++ src/app/restaurantes/restaurantes-routing.module.ts
@@ -1,9 +1,13 @@
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
+import { RestauranteAddComponent } from './restaurante-add/restaurante-add.component';
+import { RestauranteEditComponent } from './restaurante-edit/restaurante-edit.component';
 import { RestaurantesComponent } from './restaurantes/restaurantes.component';
 
 const routes: Routes = [
-  { path: '', component: RestaurantesComponent }
+  { path: '', component: RestaurantesComponent },
+  { path: 'add', component: RestauranteAddComponent },
+  { path: 'edit/:id', component: RestauranteEditComponent }
 ];
 
 @NgModule({
--- src/app/restaurantes/restaurantes.module.ts
+++ src/app/restaurantes/restaurantes.module.ts
@@ -3,15 +3,26 @@ import { CommonModule } from '@angular/common';
 
 import { RestaurantesRoutingModule } from './restaurantes-routing.module';
 import { RestaurantesComponent } from './restaurantes/restaurantes.component';
-import { PoModule, PoPageModule } from '@po-ui/ng-components';
+import { PoButtonModule, PoContainerModule, PoDividerModule, PoFieldModule, PoLoadingModule, PoModule, PoPageModule, PoTableModule } from '@po-ui/ng-components';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { RestauranteEditComponent } from './restaurante-edit/restaurante-edit.component';
+import { RestauranteAddComponent } from './restaurante-add/restaurante-add.component';
 
 
 @NgModule({
-  declarations: [RestaurantesComponent],
+  declarations: [RestaurantesComponent, RestauranteEditComponent, RestauranteAddComponent],
   imports: [
     CommonModule,
     RestaurantesRoutingModule,
-    PoPageModule
+    PoPageModule,
+    PoDividerModule,
+    PoTableModule,
+    ReactiveFormsModule,
+    FormsModule,
+    PoFieldModule,
+    PoContainerModule,
+    PoButtonModule,
+    PoLoadingModule
   ]
 })
 export class RestaurantesModule { }
